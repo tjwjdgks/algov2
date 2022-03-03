dec383b5d59af420ea8dbddc62c3899fc9107a77
tjwjdgks <tjwjdgks43@gmail.com>
2022-03-03 13:01:18 +0900

    daily algorithm

--- src/study/algorithm/PROGRAMMERS/kakao2022/five.java
+++ src/study/algorithm/PROGRAMMERS/kakao2022/five.java
@@ -1,9 +1,40 @@
 package study.algorithm.PROGRAMMERS.kakao2022;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class five {
     public int solution(int[] info, int[][] edges) {
-        int answer = 0;
-        boolean [] check = new boolean[info.length];
-        return answer;
+        int n = info.length, cnt = 0;
+        List<Integer> adj[] = new ArrayList[n];
+
+        for(int i=0; i<n; i++)
+            adj[i] = new ArrayList<>();
+
+        for(int e[] : edges) {
+            adj[e[0]].add(e[1]);
+            adj[e[1]].add(e[0]);
+        }
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {1, 0, 1});
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            cnt = Math.max(cnt, cur[0]);
+            for(int i=0, bit=1; i<n && bit<=cur[2]; bit=1<<++i) {
+                if((cur[2]&bit) > 0) {
+                    for(int j : adj[i]) {
+                        if((cur[2]&1<<j) > 0 || info[j] == 1 && cur[0]-cur[1] == 1) continue;
+                        int next[] = new int[3];
+                        next[0] = info[j] == 0 ? cur[0]+1 : cur[0];
+                        next[1] = info[j] == 1 ? cur[1]+1 : cur[1];
+                        next[2] = cur[2]^1<<j;
+                        q.add(next);
+                    }
+                }
+            }
+        }
+        return cnt;
     }
 }
--- src/study/algorithm/PROGRAMMERS/kakao2022/kakao2022Main.java
+++ src/study/algorithm/PROGRAMMERS/kakao2022/kakao2022Main.java
@@ -5,7 +5,7 @@ import study.algorithm.PROGRAMMERS.winter_summer.Area;
 public class kakao2022Main {
 
     public static void main(String[] rgs) throws Exception {
-       four four = new four();
-        four.solution(9,new int[]{0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1});
+       five five = new five();
+        five.solution(new int[]{0,0,1,1,1,0,1,0,1,0,1,1},new int[][]{{0,1},{1,2},{1,4},{0,8},{8,7},{9,10},{9,11},{4,3},{6,5},{4,6},{8,9}});
     }
 }
