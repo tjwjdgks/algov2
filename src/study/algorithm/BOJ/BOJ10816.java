f4762365807c46bfe197ff4b4606616e6cef0e3f
tjwjdgks <tjwjdgks43@gmail.com>
2022-04-01 20:49:57 +0900

    daily algorithm

---
+++ src/study/algorithm/BOJ/BOJ10816.java
@@ -0,0 +1,44 @@
+package study.algorithm.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class BOJ10816 {
+    public static boolean binarySearch(int [] arr ,int target){
+        int left = 0;
+        int right = arr.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(target == arr[mid])
+                return true;
+            else if(arr[mid]<target)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return false;
+    }
+    public static void getAnswer() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] oarr = Arrays.stream(br.readLine().split(" ")).mapToInt(i->Integer.parseInt(i)).toArray();
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        Arrays.stream(oarr).forEach(i->hm.put(i,hm.getOrDefault(i,0)+1));
+        int M = Integer.parseInt(br.readLine());
+        int [] tarr = Arrays.stream(br.readLine().split(" ")).mapToInt(i->Integer.parseInt(i)).toArray();
+        Arrays.sort(oarr);
+
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<tarr.length;i++){
+            if(binarySearch(oarr,tarr[i])){
+                sb.append(hm.get(tarr[i])+" ");
+            }
+            else
+                sb.append("0 ");
+        }
+        System.out.println(sb.toString());
+    }
+}
