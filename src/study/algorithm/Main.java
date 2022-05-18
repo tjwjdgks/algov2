df17dd711bc112050cbe6b70c229e898e47d9028
tjwjdgks <tjwjdgks43@gmail.com>
2022-05-18 10:19:40 +0900

    check intellij console

--- src/study/algorithm/Main.java
+++ src/study/algorithm/Main.java
@@ -3,12 +3,18 @@ package study.algorithm;
 import study.algorithm.BOJ.*;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] rgs) throws Exception {
-        BOJ1744 boj1744 = new BOJ1744();
-        boj1744.getAnswer();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> lines = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            lines.add(reader.readLine());
+        }
+        System.out.println(lines);
     }
 }
 
