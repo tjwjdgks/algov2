c4d55a65b6f64f2e81666480b952ee008dc40c95
tjwjdgks <tjwjdgks43@gmail.com>
2022-06-09 22:20:18 +0900

    daily algorithm

---
+++ src/study/algorithm/BOJ/BOJ2048.java
@@ -0,0 +1,22 @@
+package study.algorithm.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2048 {
+    public static void getAnswer() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(i->Integer.parseInt(i)).toArray();
+        Arrays.sort(arr);
+
+        if(arr[0] == arr[1] && arr[1] == arr[2])
+            System.out.println(10000+ arr[0]*1000);
+        else if(arr[0] == arr[1] || arr[1] == arr[2])
+            System.out.println(1000+ arr[1]*100);
+        else
+            System.out.println(arr[2]*100);
+
+    }
+}
