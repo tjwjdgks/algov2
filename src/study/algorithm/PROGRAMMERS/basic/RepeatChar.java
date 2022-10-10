76debc1e75397ac8428721388c86998fda061c5e
tjwjdgks <tjwjdgks43@gmail.com>
2022-10-10 20:14:17 +0900

    daily algorithm

--- src/study/algorithm/PROGRAMMERS/basic/RepeatChar.java
+++ src/study/algorithm/PROGRAMMERS/basic/RepeatChar.java
@@ -1,2 +1,16 @@
-package study.algorithm.PROGRAMMERS.basic;public class RepeatChar {
+package study.algorithm.PROGRAMMERS.basic;
+
+public class RepeatChar {
+    public String solution(String my_string, int n) {
+
+        String [] array = my_string.split("");
+        String answer = "";
+        for(int i=0;i<array.length;i++){
+            for(int j =0;j<n;j++){
+                answer += array[i];
+            }
+        }
+
+        return answer;
+    }
 }
