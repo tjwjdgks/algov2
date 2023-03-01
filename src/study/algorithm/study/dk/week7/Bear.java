f7bbcc3580ab3f23367a41c1a520140030c6dd8b
tjwjdgks <tjwjdgks43@gmail.com>
2023-03-01 23:38:52 +0900

    daily algorithm 개선

--- src/study/algorithm/study/dk/week7/Bear.java
+++ src/study/algorithm/study/dk/week7/Bear.java
@@ -25,33 +25,33 @@ public class Bear {
             int weight = Integer.parseInt(st.nextToken());
             edge[start].add(new int[]{end,weight});
         }
-        boolean [] check = new boolean[V+1];
+
         int [] minDistance = new int[V+1];
-        for(int i=0;i<minDistance.length;i++) minDistance[i] = -1;
+        Arrays.fill(minDistance,Integer.MAX_VALUE);
 
         Queue<int[]> pq = new PriorityQueue<>((o1,o2)->{
            if(o1[1] == o2[1]) return o1[0] - o2[0];
            return o1[1] - o2[1];
         });
+        minDistance[K] = 0;
         pq.add(new int[]{K,0});
         int count = 0;
         while(!pq.isEmpty()){
             int[] index = pq.poll();
-            if(check[index[0]]) continue;
-            check[index[0]] = true;
-            minDistance[index[0]] = index[1];
+            if(minDistance[index[0]]<index[1]) continue;
             count++;
             if(count == V) break;
             for(int i=0;i<edge[index[0]].size();i++){
                 int [] curIndex = edge[index[0]].get(i);
-                if(check[curIndex[0]] == false){
+                if(minDistance[curIndex[0]] > index[1]+curIndex[1]){
+                    minDistance[curIndex[0]] = index[1]+curIndex[1];
                     pq.add(new int[]{curIndex[0],index[1]+curIndex[1]});
                 }
             }
         }
         StringBuffer sb = new StringBuffer();
         for(int i=1;i<minDistance.length;i++){
-            String cur = (minDistance[i] == -1) ? "INF" : String.valueOf(minDistance[i]);
+            String cur = (minDistance[i] == Integer.MAX_VALUE) ? "INF" : String.valueOf(minDistance[i]);
             sb.append(cur).append("\n");
         }
         System.out.println(sb.toString());
