cafaa2958225ba085054a69efad2842112841975
tjwjdgks <tjwjdgks43@gmail.com>
2023-04-04 08:53:56 +0900

    easy algorithm

---
+++ src/study/algorithm/leetCode/P704.java
@@ -0,0 +1,21 @@
+package study.algorithm.leetCode;
+
+public class P704 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return -1;
+    }
+}
