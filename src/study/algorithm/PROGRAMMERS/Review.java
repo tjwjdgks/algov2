811258f6ff921e11ec66dc2fdd282105f794bcc8
tjwjdgks <tjwjdgks43@gmail.com>
2023-08-01 23:00:07 +0900

    daily algo

---
+++ src/study/algorithm/PROGRAMMERS/Review.java
@@ -0,0 +1,24 @@
+package study.algorithm.PROGRAMMERS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Review {
+    public int solution(int[][] scores) {
+        int[] s = scores[0];
+        Arrays.sort(scores, (s1, s2) -> s1[0] == s2[0] ? s1[1] - s2[1] : s2[0] - s1[0]);
+        int max = 0, ans = 1;
+        for(int[] score : scores) {
+            if(score[1] < max) {
+                if(score.equals(s))
+                    return -1;
+            }
+            else {
+                max = Math.max(max, score[1]);
+                if(s[0] + s[1] < score[0] + score[1])
+                    ans++;
+            }
+        }
+        return ans;
+    }
+}
