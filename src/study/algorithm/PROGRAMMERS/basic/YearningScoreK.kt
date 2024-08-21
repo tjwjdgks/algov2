027285417d577c604570615dd2861f5ac7c7a983
tjwjdgks <tjwjdgks43@gmail.com>
2024-08-21 23:58:27 +0900

    easy practice algorithm

---
+++ src/study/algorithm/PROGRAMMERS/basic/YearningScoreK.kt
@@ -0,0 +1,8 @@
+package study.algorithm.PROGRAMMERS.basic
+
+class YearningScoreK {
+    fun solution(name: Array<String>, yearning: IntArray, photos: Array<Array<String>>): IntArray {
+        val map = name.zip(yearning.toTypedArray()).toMap()
+        return photos.map { photo -> photo.sumOf { map[it] ?: 0 } }.toIntArray()
+    }
+}
