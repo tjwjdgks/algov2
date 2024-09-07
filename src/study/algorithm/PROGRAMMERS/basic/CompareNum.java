0bb0768aa86eaf238c6c1ee46f3850bdda4c2bf6
tjwjdgks <tjwjdgks43@gmail.com>
2024-09-07 18:51:00 +0900

    easy practice algorithm

---
+++ src/study/algorithm/PROGRAMMERS/basic/CompareNum.java
@@ -0,0 +1,7 @@
+package study.algorithm.PROGRAMMERS.basic;
+
+public class CompareNum {
+    public int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+    }
+}
---
+++ src/study/algorithm/PROGRAMMERS/basic/CompareNumK.kt
@@ -0,0 +1,7 @@
+package study.algorithm.PROGRAMMERS.basic
+
+class CompareNumK {
+    fun solution(num1: Int, num2: Int): Int {
+        return if (num1 == num2) 1 else -1
+    }
+}
