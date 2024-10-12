ad75caff2c06137a5912c7b09846ef9f0521fbfd
tjwjdgks <tjwjdgks43@gmail.com>
2024-10-12 16:49:45 +0900

    easy practice algorithm

--- src/study/algorithm/PROGRAMMERS/basic/CompareNum2.java
+++ src/study/algorithm/PROGRAMMERS/basic/CompareNum2.java
@@ -1,6 +1,6 @@
 package study.algorithm.PROGRAMMERS.basic;
 
-public class CompareNum {
+public class CompareNum2 {
     public int solution(int num1, int num2) {
         return num1 == num2 ? 1 : -1;
     }
